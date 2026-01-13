035aa830e952c83bd4344df1ccac668d0aa0727b
Daniel Ball <dball139@byu.edu>
2026-01-12 21:39:54 -0700

    2/3rds into phase 0 video

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,8 +8,9 @@ package chess;
  */
 public class ChessBoard {
 
+    ChessPiece[][] gameboard = new ChessPiece[8][8];
+
     public ChessBoard() {
-        
     }
 
     /**
@@ -19,7 +20,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        gameboard[position.getRow()-1][position.getColumn()-1] = piece;
     }
 
     /**
@@ -30,7 +31,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return gameboard[position.getRow()-1][position.getColumn()-1];
     }
 
     /**
