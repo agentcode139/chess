da0083002351fa15231edda1781f41fcaffc128f
Daniel Ball <dball139@byu.edu>
2026-02-09 13:49:02 -0700

    removed private method format for Bishop, Queen, and Rook moveCalculator

--- shared/src/main/java/chess/movecalculator/BishopMoveCalculator.java
+++ shared/src/main/java/chess/movecalculator/BishopMoveCalculator.java
@@ -10,35 +10,31 @@ public class BishopMoveCalculator implements ChessMoveCalculator {
     public Collection<ChessMove> moveCalculator(ChessBoard board, ChessPosition position){
         Set<ChessMove> validMoves = new HashSet<>();
         for (int d = 1; d < 5; d++) {
-            validMoves.addAll(validMovesInDir(board,position,d));
+            int r;
+            int c = switch (d) {
+                case 1 -> {
+                    r = 1;
+                    yield 1;
+                }
+                case 2 -> {
+                    r = -1;
+                    yield 1;
+                }
+                case 3 -> {
+                    r = -1;
+                    yield -1;
+                }
+                case 4 -> {
+                    r = 1;
+                    yield -1;
+                }
+                default -> {
+                    r = 0;
+                    yield 0;
+                }
+            };
+            validMoves.addAll(new LineMoveCalculator().validMovesOnLine(board,position,r,c));
         }
         return validMoves;
-    }
-
-    private Collection<ChessMove> validMovesInDir(ChessBoard board, ChessPosition position, int direction){
-        int r;
-        int c = switch (direction) {
-            case 1 -> {
-                r = 1;
-                yield 1;
-            }
-            case 2 -> {
-                r = -1;
-                yield 1;
-            }
-            case 3 -> {
-                r = -1;
-                yield -1;
-            }
-            case 4 -> {
-                r = 1;
-                yield -1;
-            }
-            default -> {
-                r = 0;
-                yield 0;
-            }
-        };
-        return new LineMoveCalculator().validMovesOnLine(board,position,r,c);
     }
 }
--- shared/src/main/java/chess/movecalculator/QueenMoveCalculator.java
+++ shared/src/main/java/chess/movecalculator/QueenMoveCalculator.java
@@ -13,52 +13,48 @@ public class QueenMoveCalculator implements ChessMoveCalculator {
     public Collection<ChessMove> moveCalculator(ChessBoard board, ChessPosition position){
         Set<ChessMove> validMoves = new HashSet<>();
         for (int d = 1; d < 9; d++) {
-            validMoves.addAll(validMovesInDir(board,position,d));
+            int r;
+            int c = switch (d) {
+                case 1 -> {
+                    r = 1;
+                    yield 0;
+                }
+                case 2 -> {
+                    r = 0;
+                    yield 1;
+                }
+                case 3 -> {
+                    r = 0;
+                    yield -1;
+                }
+                case 4 -> {
+                    r = -1;
+                    yield 0;
+                }
+                case 5 -> {
+                    r = 1;
+                    yield 1;
+                }
+                case 6 -> {
+                    r = -1;
+                    yield 1;
+                }
+                case 7 -> {
+                    r = 1;
+                    yield -1;
+                }
+                case 8 -> {
+                    r = -1;
+                    yield -1;
+                }
+
+                default -> {
+                    r = 0;
+                    yield 0;
+                }
+            };
+            validMoves.addAll(new LineMoveCalculator().validMovesOnLine(board,position,r,c));
         }
         return validMoves;
-    }
-
-    private Collection<ChessMove> validMovesInDir(ChessBoard board, ChessPosition position, int direction){
-        int r;
-        int c = switch (direction) {
-            case 1 -> {
-                r = 1;
-                yield 0;
-            }
-            case 2 -> {
-                r = 0;
-                yield 1;
-            }
-            case 3 -> {
-                r = 0;
-                yield -1;
-            }
-            case 4 -> {
-                r = -1;
-                yield 0;
-            }
-            case 5 -> {
-                r = 1;
-                yield 1;
-            }
-            case 6 -> {
-                r = -1;
-                yield 1;
-            }
-            case 7 -> {
-                r = 1;
-                yield -1;
-            }
-            case 8 -> {
-                r = -1;
-                yield -1;
-            }
-
-            default -> {
-                r = 0;
-                yield 0;
-            }
-        };
-        return new LineMoveCalculator().validMovesOnLine(board,position,r,c);
     }
 }
