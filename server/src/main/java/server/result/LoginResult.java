96402dd180262e4f1d8876410d054603c2df1b9f
Daniel Ball <dball139@byu.edu>
2026-03-02 17:58:27 -0700

    Removed unused function

--- server/src/main/java/server/result/LoginResult.java
+++ server/src/main/java/server/result/LoginResult.java
@@ -1,10 +1,5 @@
 package server.result;
 
-import dataaccess.AuthData;
-
 public record LoginResult(String username,
                           String authToken) {
-    public static LoginResult from(AuthData authData) {
-        return new LoginResult(authData.username(), authData.authToken());
-    }
 }
