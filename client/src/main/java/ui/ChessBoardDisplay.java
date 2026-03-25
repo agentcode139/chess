fd3df357072b25a159506bb62292d200988d73d6
Daniel Ball <dball139@byu.edu>
2026-03-25 02:46:49 -0600

    removed setBlack()

--- client/src/main/java/ui/ChessBoardDisplay.java
+++ client/src/main/java/ui/ChessBoardDisplay.java
@@ -92,11 +92,6 @@ public class ChessBoardDisplay {
         out.print(SET_TEXT_COLOR_WHITE);
     }
 
-    private static void setBlack(PrintStream out) {
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_BLACK);
-    }
-
     private static void setRed(PrintStream out) {
         out.print(SET_BG_COLOR_RED);
         out.print(SET_TEXT_COLOR_BLACK);
