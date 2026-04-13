d5896313bc7f84507d96b1a8eb1d4c828abaccbe
Daniel Ball <dball139@byu.edu>
2026-04-13 13:09:14 -0600

    cleaned long method declarations

--- server/src/main/java/server/websockets/WebSocketHandler.java
+++ server/src/main/java/server/websockets/WebSocketHandler.java
@@ -46,12 +46,13 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
     @Override
     public void handleMessage(@NotNull WsMessageContext ctx) {
-        //System.out.println("Websocket handling message in Handler");
         try {
-            UserGameCommand userGameCommand = new Gson().fromJson(ctx.message(), UserGameCommand.class);
+            UserGameCommand userGameCommand = new Gson().fromJson(ctx.message(),
+                    UserGameCommand.class);
             switch (userGameCommand.getCommandType()) {
                 case CONNECT -> connect(userGameCommand, ctx.session);
-                case MAKE_MOVE -> makeMove(new Gson().fromJson(ctx.message(), MakeMoveCommand.class), ctx.session);
+                case MAKE_MOVE -> makeMove(new Gson().fromJson(ctx.message(), MakeMoveCommand.class),
+                        ctx.session);
                 case LEAVE -> leave(userGameCommand, ctx.session);
                 case RESIGN -> resign(userGameCommand, ctx.session);
             }
@@ -70,19 +71,22 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
             var loadGame = new LoadGameMessage(gameData.game());
             connections.broadcastTo(session, loadGame);
 
-            // Server sends a Notification message to all other clients in that game informing them the root client connected to the game, either as a player (in which case their color must be specified) or as an observer.
             String joinType;
-            if (gameData.blackUsername() != null && username.equals(gameData.blackUsername())) {
+            if (gameData.blackUsername() != null &&
+                    username.equals(gameData.blackUsername())) {
                 joinType = "black player";
-            } else if (gameData.whiteUsername() != null && username.equals(gameData.whiteUsername())) {
+            } else if (gameData.whiteUsername() != null &&
+                    username.equals(gameData.whiteUsername())) {
                 joinType = "white player";
             } else {
                 joinType = "observer";
             }
-            gameClients.computeIfAbsent(userGameCommand.getGameID(), k -> ConcurrentHashMap.newKeySet()).add(session);
+            gameClients.computeIfAbsent(userGameCommand.getGameID(),
+                    k -> ConcurrentHashMap.newKeySet()).add(session);
 
             var notification = new NoticationMessage(username + " has connected to game as " + joinType);
-            connections.broadcastExclusiveInclude(session, gameClients.get(userGameCommand.getGameID()), notification);
+            connections.broadcastExclusiveInclude(session,
+                    gameClients.get(userGameCommand.getGameID()), notification);
         } catch (Exception e) {
             connections.broadcastTo(session, new ErrorMessage("Failed Connect:" + e.getMessage()));
         }
@@ -90,7 +94,6 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
     private void makeMove(MakeMoveCommand makeMoveCommand, Session session) throws IOException {
         try {
-            // Server verifies the validity of the move.
             String username = service.getUserName(makeMoveCommand.getAuthToken());
             GameData gameData = service.getGame(makeMoveCommand.getGameID());
             ChessGame game = gameData.game();
@@ -112,19 +115,17 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
                 throw new InvalidMoveException();
             }
 
-            // Game is updated to represent the move. Game is updated in the database.
             game.makeMove(move);
             service.updateGame(gameData);
 
-            // Server sends a LOAD_GAME message to all clients in the game (including the root client) with an updated game.
             var loadGame = new LoadGameMessage(gameData.game());
-            connections.broadcastExclusiveInclude(null, gameClients.get(makeMoveCommand.getGameID()), loadGame);
+            connections.broadcastExclusiveInclude(null,
+                    gameClients.get(makeMoveCommand.getGameID()), loadGame);
 
-            // Server sends a Notification message to all other clients in that game informing them what move was made.
             NoticationMessage notificationMove = new NoticationMessage("move " + move + " was made");
-            connections.broadcastExclusiveInclude(session, gameClients.get(makeMoveCommand.getGameID()), notificationMove);
+            connections.broadcastExclusiveInclude(session,
+                    gameClients.get(makeMoveCommand.getGameID()), notificationMove);
 
-            // If the move results in check, checkmate or stalemate the server sends a Notification message to all clients.
             NoticationMessage notification = null;
             if (game.isInCheck(ChessGame.TeamColor.WHITE)) {
                 notification = new NoticationMessage("White is in check");
@@ -155,16 +156,16 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
     private void leave(UserGameCommand userGameCommand, Session session) throws IOException {
         try {
-            // If a player is leaving, then the game is updated to remove the root client. Game is updated in the database.
             String username = service.getUserName(userGameCommand.getAuthToken());
             GameData gameData = service.getGame(userGameCommand.getGameID());
 
-            Set<Session> clients = gameClients.getOrDefault(userGameCommand.getGameID(), ConcurrentHashMap.newKeySet());
+            Set<Session> clients = gameClients.getOrDefault(userGameCommand.getGameID(),
+                    ConcurrentHashMap.newKeySet());
             clients.remove(session);
             String playerColor = (Objects.equals(gameData.whiteUsername(), username)) ? "WHITE" : "BLACK";
-            service.leaveGame(userGameCommand.getAuthToken(), new JoinGameRequest(playerColor, userGameCommand.getGameID()));
+            service.leaveGame(userGameCommand.getAuthToken(),
+                    new JoinGameRequest(playerColor, userGameCommand.getGameID()));
 
-            // Server sends a Notification message to all other clients in that game informing them that the root client left. This applies to both players and observers
             connections.broadcastExclusiveInclude(session,
                     gameClients.get(userGameCommand.getGameID()),
                     new NoticationMessage(username + " left " + gameData.gameName()));
@@ -176,21 +177,20 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
     private void resign(UserGameCommand userGameCommand, Session session) throws IOException {
         try {
-            // Server marks the game as over (no more moves can be made). Game is updated in the database.
             String username = service.getUserName(userGameCommand.getAuthToken());
             GameData gameData = service.getGame(userGameCommand.getGameID());
             ChessGame game = gameData.game();
             if (!game.isActive()) {
                 throw new InvalidMoveException();
             }
-            if (!Objects.equals(username, gameData.whiteUsername()) && !Objects.equals(username, gameData.blackUsername())) {
+            if (!Objects.equals(username, gameData.whiteUsername()) &&
+                    !Objects.equals(username, gameData.blackUsername())) {
                 throw new InvalidMoveException();
             }
 
             game.setActive(false);
             service.updateGame(gameData);
 
-            // Server sends a Notification message to all clients in that game informing them that the root client resigned. This applies to both players and observers.
             connections.broadcastExclusiveInclude(null,
                     gameClients.get(userGameCommand.getGameID()),
                     new NoticationMessage(username + " has resigned"));
